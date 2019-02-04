21e31280f93759f8865fbd216dcd7f0dda29cb64
Sil van Diepen <me@silvandiepen.com>
2019-02-04 16:13:07 +0100

    add routeid to link

--- pages/404.vue
+++ pages/404.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="container">
-		<nuxt-link to="/">
+		<nuxt-link :to="'/compare/' + routeId">
 			Home page
 		</nuxt-link>
 	</div>
@@ -9,13 +9,21 @@
 <script>
 export default {
 	props: ['error'],
+	data() {
+		return {
+			routeId: null
+		};
+	},
 	created() {
 		if (this.$route.id) {
+			this.routeId = this.$route.id;
 			if (this.$route.id.split('-').length > 0) {
 				// this.$route.push('compare', { id: this.$route.id });
+
 				this.$router.push({ name: 'compare', query: { id: this.$route.id } });
 			}
 		}
+		console.log(this.$route);
 		this.$store.dispatch('passport/setCountryList');
 		console.log('created-route', this.$route);
 		console.log('created-props', this.$props);
